1fbc48e859515d7f47cd388297567a901b1123a7
Andreas Schulz <andi.schulz@me.com>
2017-05-12 02:10:45 +0200

    first examples

---
+++ README.md
@@ -0,0 +1,12 @@
+# qlift-c-api-example
+Example C project using the C API to Qt for Qlift
+
+# License
+
+## This Project
+
+MIT
+
+## Verdigris
+
+LGPLv3
---
+++ src/main.c
@@ -0,0 +1,15 @@
+#ifdef __cplusplus
+#error No c++
+#endif
+
+#include "qlift-c-api.h"
+
+int main(int argc, char *argv[]) {
+    void *app = QApplication_ctor(argc, argv);
+    void *mainWindow = QMainWindow_ctor_parent_default_flags_default();
+    void *label = QLabel_ctor_text_arg1_parent_arg2_flags_default("Hello, world!", mainWindow);
+    QLabel_setAlignment(label, 132);
+    QMainWindow_setCentralWidget(mainWindow, label);
+    QMainWindow_show(mainWindow);
+    return QApplication_exec(app);
+}
