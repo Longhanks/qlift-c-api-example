dd19eb0824f77df077ea8817faee081c162a13f0
Andreas Schulz <andi.schulz@me.com>
2017-07-06 20:00:00 +0200

    Use argc via pointer

--- src/main.c
+++ src/main.c
@@ -5,11 +5,12 @@
 #include "qlift-c-api.h"
 
 int main(int argc, char *argv[]) {
-    void *app = QApplication_ctor(argc, argv);
+    void *app = QApplication_ctor(&argc, argv);
     void *mainWindow = QMainWindow_ctor_parent_default_flags_default();
     void *label = QLabel_ctor_text_arg1_parent_arg2_flags_default("Hello, world!", mainWindow);
     QLabel_setAlignment(label, 132);
     QMainWindow_setCentralWidget(mainWindow, label);
     QMainWindow_show(mainWindow);
-    return QApplication_exec(app);
+    int result = QApplication_exec(app);
+    return result;
 }
