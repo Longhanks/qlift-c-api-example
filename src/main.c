cd7de10c5ee31d1bb61f6c5b60c098e3fc07bbd9
Andreas Schulz <andi.schulz@me.com>
2017-07-13 22:00:00 +0200

    Update to qlift-c-api 0.0.13

--- src/main.c
+++ src/main.c
@@ -13,9 +13,15 @@ void slot(int checked) {
 int main(int argc, char *argv[]) {
     void *application = QApplication_new(&argc, argv);
     void *mainWindow = QMainWindow_new(NULL, 0);
+    void *widget = QWidget_new(NULL, 0);
+    void *layout = QVBoxLayout_new(widget);
+    void *label = QLabel_new("Hello, world!", NULL, 0);
+    QLabel_setAlignment(label, 132);
     void *pushButton = QPushButton_new(NULL, "Push me!", mainWindow);
     QAbstractButton_clicked_connect(pushButton, pushButton, &slot);
-    QMainWindow_setCentralWidget(mainWindow, pushButton);
+    QBoxLayout_addWidget(layout, label, 0, 0);
+    QBoxLayout_addWidget(layout, pushButton, 0, 0);
+    QMainWindow_setCentralWidget(mainWindow, widget);
     QWidget_show(mainWindow);
     int result = QCoreApplication_exec(application);
     return result;
